e77726a4967dfc3c124bba6bd18b5ccc532e1ec0
v3_merging_lms_dac <sureshatpure@gmail.com>
2015-10-23 13:08:27 +0530

    developing lms_dac status and substatus option in this module

--- application/config/database.php
+++ application/config/database.php
@@ -49,20 +49,21 @@
 $active_group = "forms";
 $active_record = TRUE;
 
+
+
+$db['forms']['hostname'] = '10.1.9.71';
+$db['forms']['username'] = 'puredb';
+$db['forms']['password'] = 'pure123';
+$db['forms']['database'] = 'softcodeformtest_lms';
+$db['forms']['dbdriver'] = 'postgre';
+
+/*
 $db['forms']['hostname'] = '10.1.9.71';
 $db['forms']['username'] = 'puredb';
 $db['forms']['password'] = 'pure123';
 $db['forms']['database'] = 'softcodeformstest_pg';
 $db['forms']['dbdriver'] = 'postgre';
 
-
-
-/*
-$db['forms']['hostname'] = '10.1.9.71';
-$db['forms']['username'] = 'puredb';
-$db['forms']['password'] = 'pure123';
-$db['forms']['database'] = 'softcodeformtest_lms';
-$db['forms']['dbdriver'] = 'postgre';
 
 $db['forms']['hostname'] = '10.1.0.21';
 $db['forms']['username'] = 'puredb';
@@ -97,19 +98,19 @@ $db['forms']['port'] 	  = '5432';
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = '10.1.9.71';
-$db['default']['username'] = 'puredb';
-$db['default']['password'] = 'pure123';
-$db['default']['database'] = 'softcodetest_pg';
-$db['default']['dbdriver'] = 'postgre';
 
-/*
 $db['default']['hostname'] = '10.1.9.71';
 $db['default']['username'] = 'puredb';
 $db['default']['password'] = 'pure123';
 $db['default']['database'] = 'softcodetest_lms';
 $db['default']['dbdriver'] = 'postgre';
 
+/*
+$db['default']['hostname'] = '10.1.9.71';
+$db['default']['username'] = 'puredb';
+$db['default']['password'] = 'pure123';
+$db['default']['database'] = 'softcodetest_pg';
+$db['default']['dbdriver'] = 'postgre';
 
 $db['default']['hostname'] = '10.1.0.21';
 $db['default']['username'] = 'puredb';
